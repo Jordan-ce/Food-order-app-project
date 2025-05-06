0db8ac8a13cab983395fb63fdceacd442f132ace
Jordan <84409444+Jordan-ce@users.noreply.github.com>
2025-05-06 11:14:02 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:3000/meals")
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
+}
+
+export default App;
